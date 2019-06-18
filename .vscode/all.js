6877bccab3e20b7af13660e71095e5f6d5e28ae3
Fran Zekan <zekan.fran369@gmail.com>
2019-06-18 18:27:20 +0200

    Add code init

---
+++ .vscode/all.js
@@ -0,0 +1,6 @@
+// chrome.webNavigation.onHistoryStateUpdated.addListener(() => { alert("a") });
+
+// console.log(tab);
+  // console.log(document.location.href);
+  // console.log(chrome.extension.getBackgroundPage().document.location.href);
+  // console.log('Reload page');
---
+++ content2.js
@@ -0,0 +1,5 @@
+// console.log("2");
+// window.location.replace("https://www.youtube.com/feed/subscriptions");
+
+console.log(document.location.href);
+console.log('Home page or else');
